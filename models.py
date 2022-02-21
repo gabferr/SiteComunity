3402a7c0da45209e29914beb75049561f8208426
gabferr <gabrielferrandin@hotmail.com>
2022-02-21 10:27:44 -0300

    Ultimo commit

--- models.py
+++ models.py
@@ -2,16 +2,19 @@ from main import database
 from datetime import datetime
 
 
-class Usuario(database.Model):
-    id = database.Column(database.Integer, Primaty_key=True)
-    username = database.Column(database.String, nulabble=False)
-    email = database.Column(database.String, nulabble=False, unique=True)
-    senha = database.Column(database.String, nulabble=False)
-    foto_perfil = database.Column(database.String, default='default.jpg', nulabble=False)
+class Usuario(database.Model, 'extend_existing=True'):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    foto_perfil = database.Column(database.String, nullable=False, default='default.jpg')
+    post = database.relationship('Post', backref='autor', lazy=True)
+    cursos = database.Column(database.String, default='Usuario não informado', nullable=False)
 
 
 class Post(database.Model):
-    id = database.Column(database.Integer, Primaty_key=True)
-    titulo = database.Column(database.String, nulabble=False)
-    corpo = database.Column(database.String, nulabble=False)
-    data_criacao = database.Column(database.DateTime, nulabble=False, default=datetime.utcnow)
+    id = database.Column(database.Integer, primary_key=True)
+    titulo = database.Column(database.String, nullable=False)
+    corpo = database.Column(database.String, nullable=False)
+    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = database.Column(database.Integer, database.Foreignkey('usuario.id'), nullable=False)
